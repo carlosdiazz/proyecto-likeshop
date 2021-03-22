b9d2d282ce30a0b228c1c735a9d1d2e8553ce99e
Carlos Jose Diaz Adrian <c.diazadriann@gmail.com>
2021-03-22 18:56:51 -0400

    Terminando ruta de Home 7

--- fronted/src/app/app-routing.module.ts
+++ fronted/src/app/app-routing.module.ts
@@ -13,7 +13,7 @@ const routes: Routes = [
   },
 
   {
-    path: 'product/id', component: ProductComponent
+    path: 'product/:id', component: ProductComponent
   },
 
   {
--- fronted/src/app/services/product.service.ts
+++ fronted/src/app/services/product.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Router} from '@angular/router';
+import {environment} from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  private SERVER_URL = environment.SERVER_URL;
+
+  constructor(private http: HttpClient) { }
+
+  // Obtener todos los productos
+  mostrarMessage() {
+    console.log('Hola');
+  }
+
+  getAllProducts(numberOfResults= 10 ) {
+    return this.http.get(this.SERVER_URL + '/products', {
+      params : {
+        limit : numberOfResults.toString()
+      }
+    })
+  }
+
 }
