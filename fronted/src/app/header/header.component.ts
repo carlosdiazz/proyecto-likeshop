84cce773f45aac220aaeda828d636dce618ba553
Carlos Jose Diaz Adrian <c.diazadriann@gmail.com>
2021-04-05 22:59:46 -0400

    Termianndo el componente Header y las notificaciones

--- fronted/src/app/header/header.component.ts
+++ fronted/src/app/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CartModelServer } from '../models/cart.model';
+import { CartService } from '../services/cart.service';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  cartData: CartModelServer;
+  cartTotal: Number;
 
-  constructor() { }
+
+
+  constructor(public cartService:CartService) { }
 
   ngOnInit(): void {
+    this.cartService.cartTotal$.subscribe(total => {
+      this.cartTotal = total; 
+    });
+  
+    this.cartService.cartDataObs$.subscribe(data => this.cartData = data);
+
   }
+
 
 }
