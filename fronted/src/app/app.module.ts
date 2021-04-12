91ffc573a00a6075f645096a219fcc1d0be83c26
Carlos Jose Diaz Adrian <c.diazadriann@gmail.com>
2021-04-11 23:50:30 -0400

    Formulario en camino

--- fronted/src/app/app.module.ts
+++ fronted/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ThankyouComponent } from './thankyou/thankyou.component';
 import {HttpClientModule} from '@angular/common/http';
 import { NgxSpinnerModule } from 'ngx-spinner';
 import { ToastrModule } from 'ngx-toastr';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -33,7 +34,8 @@ import { ToastrModule } from 'ngx-toastr';
     AppRoutingModule,
     HttpClientModule,
     NgxSpinnerModule,
-    ToastrModule.forRoot()
+    ToastrModule.forRoot(),
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
